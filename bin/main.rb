55ed33d9fd87ed4a03891e5e6e9cabed04dcf3f3
jpdf00 <j.pdf00@gmail.com>
2021-01-22 23:36:35 -0300

    Update main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -8,30 +8,30 @@ type = "Creature"
 subtype = "Dragon"
 supertype = "Legendary"
 TOKEN = "Token"
-list = {}
 name_array = []
 link_array = []
+set_array = []
 
 
 link_1 = "http://gatherer.wizards.com/pages/search/default.aspx?page=0&output=compact&action=advanced&type=+[#{type}]+![#{supertype}]+![#{TOKEN}]&subtype=+[#{subtype}]"
-link_2 = "http://gatherer.wizards.com/pages/search/default.aspx?page=0&output=checklist&action=advanced&type=+[#{type}]+![#{supertype}]+![#{TOKEN}]&subtype=+[#{subtype}]"
-
 doc_1 = Nokogiri::HTML(URI.open(link_1))
-doc_2 = Nokogiri::HTML(URI.open(link_2))
 
-# Search for nodes by xpath
 doc_1.xpath('//td//a').each do |content|
   name_array << content.content unless content.content == ""
 end
 
 doc_1.xpath('//td//div//a//@href').each do |link|
-  i = 0
-  list[name_array[i]] << link unless link.content == ""
-  i += 1
+  link_array << link.content unless link.content == ""
 end
-puts list
 
+link_2 = "https://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=#{link_array[1]}"
+doc_2 = Nokogiri::HTML(URI.open(link_2))
 
+doc_2.css("#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_currentSetSymbol a").each do |link|
+  set_array << link.content
+end
+puts link_array
+#otherSetsRow
 #
 #doc_2 = Nokogiri::HTML(URI.open(link_2))
 #
