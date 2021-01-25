bd572cb182ad0d067ada47f81bc9b87cf02c8e28
jpdf00 <j.pdf00@gmail.com>
2021-01-25 18:10:04 -0300

    Update main.rb
    Finished code

--- bin/main.rb
+++ bin/main.rb
@@ -4,65 +4,53 @@ require 'open-uri'
 require_relative '..\lib\scraper_logic'
 
 search = Search.new
+validation = Validation.new
 
-loop do
-  flag = true
+2.times do
   term = ''
   inclusion = ''
   repeat = ''
-  Validation.chage_term
-  while flag == true do
+  validation.chage_term
+  loop do
     puts
     loop do
-      print "Chose a Card #{Validation.current_term}: "
+      print "Chose a Card #{validation.current_term}: "
       term = gets.chomp.downcase.capitalize
-      break if Validation.term_validation?(term)
-      puts
-      puts "Not a valid Card #{Validation.current_term}. "
-      puts Validation.type_list if Validation.current_term == 'Type'
-      puts Validation.type_list if Validation.current_term == 'Supertype'
-      print Validation.subtype_list if Validation.current_term == 'Subtype'
-      puts
+      break if validation.current_term == 'Subtype'
+
+      break if validation.type_list.include? term
+
+      puts "Not a valid Card #{validation.current_term}. "
+      puts "The valid Card #{validation.current_term}s are #{validation.type_list}"
     end
     loop do
       print 'Include or Exclude from the search? (I/E): '
       inclusion = gets.chomp.upcase
-      if search.inclusion_validation.include? inclusion
-        inclusion = inclusion == 'E' ? '!':''
-        break
-      else
-        puts
-        puts 'Invalid Input'
-        puts
-      end
+      break if validation.inclusion_validation.include? inclusion
+
+      puts 'Invalid Input. Chose either I or N'
     end
     loop do
-      print "Want to add another? (Y/N): "
+      print 'Want to add another? (Y/N): '
       repeat = gets.chomp.upcase
-      if search.repeat_validation.include? repeat
-        break
-      else
-        puts
-        puts 'Invalid Input'
-        puts
-      end
+      break if validation.repeat_validation.include? repeat
+
+      puts 'Invalid Input. Chose either Y or N'
     end
-    flag = false if repeat == 'N'
-    search.build_link(term, inclusion)
+    search.build_link(term, inclusion, validation.current_term)
+    break if repeat == 'N'
   end
-  break if Validation.current_term == "Subtype"
 end
 
 puts
 puts "Your search returned #{search.web_scrape.length} results."
 puts
-puts search.name_array
+show_results = 'N'
+loop do
+  print 'Want to view a list with the results? (Y/N): '
+  show_results = gets.chomp.upcase
+  break if validation.repeat_validation.include? show_results
 
-#set_array = []
-#
-#link_2 = "https://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=#{link_array[1]}"
-#doc_2 = Nokogiri::HTML(URI.open(link_2))
-#
-#doc_2.css("#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_currentSetSymbol a").each do |link|
-#  set_array << link.content
-#end
+  puts 'Invalid Input. Chose either Y or N'
+end
+puts search.name_array if show_results == 'Y'
--- lib/scraper_logic.rb
+++ lib/scraper_logic.rb
@@ -2,90 +2,67 @@
 require 'nokogiri'
 require 'open-uri'
 
-module Validation
-  @@term = ['Type', 'Supertype', 'Subtype']
-  @@current_term = ''
-  @@type_array =[]
-  @@subtype_array =[]
-  @@validation_page = Nokogiri::HTML(URI.open('https://gatherer.wizards.com/Pages/Advanced.aspx'))
-  @@inclusion_validation = ['I', 'E']
-  @@repeat_validation = ['Y', 'N']
+class Validation
+  attr_reader :current_term, :inclusion_validation, :repeat_validation
 
-  def self.type_list
-    @@validation_page.css("#autoCompleteSourceBoxtypeAddText3_InnerTextBoxcontainer a").each do |link|
-      @@type_array << link.content
+  def initialize
+    @term = %w[Type Subtype]
+    @current_term = ''
+    @type_array = []
+    @subtype_array = []
+    @validation_page = Nokogiri::HTML(URI.open('https://gatherer.wizards.com/Pages/Advanced.aspx'))
+    @inclusion_validation = %w[I E]
+    @repeat_validation = %w[Y N]
+  end
+
+  def type_list
+    @validation_page.css('#autoCompleteSourceBoxtypeAddText3_InnerTextBoxcontainer a').each do |link|
+      @type_array << link.content
     end
-    @@type_array
+    @type_array
   end
 
-  def self.subtype_list
-    @@validation_page.css("#autoCompleteSourceBoxsubtypeAddText4_InnerTextBoxcontainer a").each do |link|
-      @@subtype_array << link.content
-    end
-    @@subtype_array
-  end
-
-  def self.current_term
-    @@current_term
-  end
-
-  def self.inclusion_validation
-    @@inclusion_validation
-  end
-
-  def self.repeat_validation
-    @@repeat_validation
-  end
-
-  def self.chage_term
-    @@current_term = @@term.shift
-    @@term.push(@@current_term)
-  end
-
-  def self.term_validation?(term)
-    check = if @@current_term == 'Subtype'
-              Validation.subtype_list.include? term
-            else
-              Validation.type_list.include? term
-            end
-    check
+  def chage_term
+    @current_term = @term.shift
+    @term.push(@current_term)
   end
 end
 
 class Search
-  attr_reader :link, :name_array
+  attr_reader :link, :name_array, :validation
 
   def initialize
     @page = 0
     @base_link = 'http://gatherer.wizards.com/pages/search/default.aspx?'
-    @search_link = '&output=compact&action=advanced&type=+!["Token"]'
+    @options_link = '&output=compact&action=advanced'
+    @type_link = '&type=+!["Token"]'
+    @subtype_link = '&subtype='
     @name_array = []
     @link_array = []
     @page_array = []
   end
 
-  def build_link(term, inclusion)
-    @search_link += "&subtype=" if Validation.current_term == "Subtype"
-    @search_link += "+#{inclusion}[\"#{term}\"]"
+  def build_link(term, inclusion, current_term)
+    inclusion = inclusion == 'E' ? '!' : ''
+    @type_link += "+#{inclusion}[\"#{term}\"]" if current_term == 'Type'
+    @subtype_link += "+#{inclusion}[\"#{term}\"]" if current_term == 'Subtype'
   end
 
   def web_scrape
     loop do
-      retrieved_page = Nokogiri::HTML(URI.open("#{@base_link}page=#{@page.to_s}#{@search_link}"))
+      full_link = "#{@base_link}page=#{@page}#{@options_link}#{@type_link}#{@subtype_link}"
+      retrieved_page = Nokogiri::HTML(URI.open(full_link))
 
       retrieved_page.xpath('//td//a').each do |content|
-        @name_array << content.content unless content.content == ""
-      end
-
-      retrieved_page.xpath('//td//div//a//@href').each do |link|
-        @link_array << link.content unless link.content == ""
+        @name_array << content.content unless content.content == ''
       end
 
       retrieved_page.css('#ctl00_ctl00_ctl00_MainContent_SubContent_topPagingControlsContainer a').each do |link|
-        @page_array << link.content.to_i - 1 unless link.content.to_i == 0
+        @page_array << link.content.to_i - 1 unless link.content.to_i.zero?
       end
 
       break if @page == @page_array[-1] or @page_array.empty?
+
       @page += 1
     end
     @name_array
