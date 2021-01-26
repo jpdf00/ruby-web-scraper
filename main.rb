9ae490b656db63be06b657a2ef7b3e63469a4306
jpdf00 <j.pdf00@gmail.com>
2021-01-26 11:45:52 -0300

    Update main.rb
    Update wrapper to eork on repl.it

--- main.rb
+++ main.rb
@@ -3,3 +3,10 @@
 # wrapper to make this program work on repl.it
 
 require_relative './bin/main'
+
+require 'bundler/inline'
+
+gemfile true do
+  source 'http://rubygems.org'
+  gem 'nokogiri'
+end
