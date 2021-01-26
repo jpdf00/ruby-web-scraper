5ea07466eea49dfa68e4df7f77df4f6b4755f2cf
jpdf00 <j.pdf00@gmail.com>
2021-01-25 22:26:24 -0300

    Update scraper_logic_spec.rb

--- spec/scraper_logic_spec.rb
+++ spec/scraper_logic_spec.rb
@@ -1,7 +1,7 @@
 #! /usr/bin/env ruby
-require_relative '../lib/scraper_logic'
 require 'nokogiri'
 require 'open-uri'
+require_relative '../lib/scraper_logic'
 
 validation = Validation.new
 search = Search.new
